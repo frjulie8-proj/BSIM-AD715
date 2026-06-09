2062cc76225e76bc6aa480a06d79d28ed219787d
Sree Kumar <vbs@bu.edu>
2026-06-09 14:13:35 -0400

    updated data/ dir

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from bsim_export import export_bsim_excel
 
+import datetime
+
 # ── Edit these values from your BSIM 36 Month Pro Forma export ───────────────
 # fixed_cost : Total Fixed Cost FY1
 # var_cost   : Variable Cost per Pint (material + labor + other)
@@ -11,5 +13,5 @@ export_bsim_excel(
     var_cost=0.92,
     avg_price=3.62,
     qty_actual=2344334,
-    output_path="/Users/julie/Desktop/Code-project/BSIM Excel export/BSIM_Export_Test.xlsx"
+    output_path=f"data/BSIM_Export_Test_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx"
 )
